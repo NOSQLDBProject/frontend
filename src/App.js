f167b5b366f78a1708da8803e25e6b44c5d1a1ee
jamal1aarab <jamal.aarab.111@gmail.com>
2024-06-09 17:04:30 +0100

    loan page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import HomePage from './routes/HomePage';
 import AdherentPage from './routes/AdherentPage';
 import AddAdherentPage from './routes/AddAdherentPage';
@@ -7,6 +8,7 @@ import LivrePage from './routes/LivrePage';
 import { useParams } from 'react-router-dom';
 import Login from './routes/Login';
 import SignUp from './routes/SignUp';
+import LoanPage from './routes/LoanPage';
 
 function App() {
 
@@ -21,15 +23,18 @@ function App() {
     <Router>
       <Routes>
         {/* <Route exact path="/" render={() => (<Redirect to="/home" />)} /> */}
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<SignUp/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
 
-        <Route path="/home" element={<HomePage/>} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/adherents">
-          <Route index element={<AdherentPage/>} />
-          <Route path="add" element={<AddAdherentPage/>} />
+          <Route index element={<AdherentPage />} />
+          <Route path="add" element={<AddAdherentPage />} />
         </Route>
-        <Route path="/livre" element={<LivrePage/>} />
+        <Route path="/livre" element={<LivrePage />} />
+
+        <Route path='/loan' element={<LoanPage />} />
       </Routes>
     </Router>
   );
